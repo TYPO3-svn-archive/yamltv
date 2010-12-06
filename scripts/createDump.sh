6344d7409240f9d6350e37144914bd011da6f5da
dieterbunkerd <dieterbunkerd@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-06 06:23:03 +0000

    tx_realurl_enable is now always replaced by marker ###ENABLE_REALURL###, no matter if it is set to 0 or 1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yamltv/trunk@40844 735d13b6-9817-0410-8766-e36946ffe9aa

--- scripts/createDump.sh
+++ scripts/createDump.sh
@@ -27,14 +27,14 @@ mysql -u ${USER} --password=${PASSWORD} ${DATABASE} -e 'DROP TABLE IF EXISTS be_
 sed "s/be_users_shadow/be_users/g" ${OUTPUTFILE}_dump > ${OUTPUTFILE}_shadow
 
 # Remove character set from table copy
-sed "s/ CHARACTER SET utf8//g" ${OUTPUTFILE}_shadow> ${OUTPUTFILE}_character
-sed "s/ DEFAULT CHARSET=latin1/ DEFAULT CHARSET=utf8/g" ${OUTPUTFILE}_character> ${OUTPUTFILE}_character2
+sed "s/ CHARACTER SET utf8//g" ${OUTPUTFILE}_shadow > ${OUTPUTFILE}_character
+sed "s/ DEFAULT CHARSET=latin1/ DEFAULT CHARSET=utf8/g" ${OUTPUTFILE}_character > ${OUTPUTFILE}_character2
 
 # Comment absRefPrefix
 sed "s/absRefPrefix/\# absRefPrefix/g" ${OUTPUTFILE}_character2 > ${OUTPUTFILE}_absRefPrefix
 
 # Replace ENABLE REALURL
-sed "s/tx_realurl_enable = 1/tx_realurl_enable = \#\#\#ENABLE_REALURL\#\#\#/g" ${OUTPUTFILE}_absRefPrefix > ${OUTPUTFILE}_realURL
+sed "s/tx_realurl_enable = [1|0]/tx_realurl_enable = \#\#\#ENABLE_REALURL\#\#\#/g" ${OUTPUTFILE}_absRefPrefix > ${OUTPUTFILE}_realURL
 
 # Replace Site path
 sed "s/domain = [^\\]*/domain = ###HOSTNAME_AND_PATH###/g" ${OUTPUTFILE}_realURL > ${OUTPUTFILE}
