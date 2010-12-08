149dfb27e5ee44fb36167e6274ee6a03cebc99f2
dieterbunkerd <dieterbunkerd@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-08 11:25:49 +0000

    Fixed issue #11328, deleted translations and translations/en, updated scripts/makeYamltvPackage_v2.sh
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yamltv/trunk@40953 735d13b6-9817-0410-8766-e36946ffe9aa

--- scripts/makeYamltvPackage_v2.sh
+++ scripts/makeYamltvPackage_v2.sh
@@ -6,19 +6,17 @@
 #   * yamltv
 # 
 # Change the typo3conf/localconf.php => extList and add:
-# felogin,indexed_search,yamltv,realurl,tt_news,static_info_tables,templavoila
-# 
-# Take the file translations/en/yamltv.sql from the "yamltv" trunk and copy it to
-# typo3conf/ext/yamltv/Resources/Private/Subpackages/Yamltv/Database
+# ,yamltv
 
-packagename="yamltvpackage"
+packagename="yamltv-pre-typo3"
 packageversion="4.4.4"
+packagelocation="https://svn.typo3.org/TYPO3v4/Extensions/yamltv/trunk/yamltv/"
 
 mkdir $packagename-$packageversion
 cd $packagename-$packageversion
 
 # fetch the dummy and source package
-wget -q http://downloads.sourceforge.net/project/typo3/TYPO3%20Source%20and%20Dummy/TYPO3%20$packageversion/typo3_src+dummy-$packageversion.zip
+wget http://downloads.sourceforge.net/project/typo3/TYPO3%20Source%20and%20Dummy/TYPO3%20$packageversion/typo3_src+dummy-$packageversion.zip
 unzip -q typo3_src+dummy-$packageversion.zip
 mv typo3_src+dummy-$packageversion/* .
 rm typo3_src+dummy-$packageversion.zip
@@ -27,25 +25,44 @@ rmdir typo3_src+dummy-$packageversion
 
 # fetch yamltv package data
 # see http://forge.typo3.org/projects/extension-yamltv/repository 
-svn -q export https://svn.typo3.org/TYPO3v4/Extensions/yamltv/trunk/yamltv/ typo3conf/ext/yamltv
-#
-rmdir typo3conf/ext/introduction/Resources/Private/Subpackages/Yamltv/Database
-svn -q export https://svn.typo3.org/TYPO3v4/Extensions/yamltv/trunk/translations/en/ typo3conf/ext/yamltv/Resources/Private/Subpackages/Introduction/Database
+svn export $packagelocation typo3conf/ext/yamltv
 
 # update localconf.php
 echo "<?php
 \$TYPO3_CONF_VARS['SYS']['sitename'] = 'New TYPO3 site';
-
-	// Default password is 'joh316':
+// Default password is 'joh316':
 \$TYPO3_CONF_VARS['BE']['installToolPassword'] = 'bacb98acf97e0b6112b1d1b650b84971';
-
-\$TYPO3_CONF_VARS['EXT']['extList'] = 'tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin,t3editor,reports,felogin,indexed_search,yamltv,realurl,tt_news,static_info_tables,templavoila';
-
+\$TYPO3_CONF_VARS['EXT']['extList'] = 'tsconfig_help,context_help,extra_page_cm_options,impexp,sys_note,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,func_wizards,wizard_crpages,wizard_sortpages,lowlevel,install,belog,beuser,aboutmodules,setup,taskcenter,info_pagetsconfig,viewpage,rtehtmlarea,css_styled_content,t3skin,t3editor,reports,felogin,indexed_search,yamltv';
 \$typo_db_extTableDef_script = 'extTables.php';
-
 ## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
-
 ?>" > typo3conf/localconf.php
+
+# update extTables.php
+echo "<?php
+/**
+ * Overriding $TCA
+ *
+ * The TYPO3 Configuration Array (TCA) is defined by the distributed tables.php and ext_tables.php files.
+ * If you want to extend and/or modify its content, you can do so with scripts like this.
+ * Or BETTER yet - with extensions like those found in the typo3conf/ext/ or typo3/ext/ folder.
+ * Extensions are movable to other TYPO3 installations and provides a much better division between things! Use them!
+ *
+ * Information on how to set up tables is found in the document "Inside TYPO3" available as a PDF from where you downloaded TYPO3.
+ *
+ * Usage:
+ * Just put this file to the location typo3conf/extTables.php and add this line to your typo3conf/localconf.php:
+ * \$typo_db_extTableDef_script = 'extTables.php';
+ */
+
+\$TCA['tt_content']['columns']['colPos']['config']['items'] = array (
+  '0' => array ('Main content||Main content||||||||','0'),
+	'1' => array ('Side content||Side content||||||||','1'),
+	'2' => array ('Special content||Special content||||||||','2'),
+	'3' => array ('Ext content left||Ext content left||||||||','3'),
+	'4' => array ('Ext content middle||Ext content middle||||||||','4'),
+	'5' => array ('Ext content right||Ext content right||||||||','5')
+);
+?>" > typo3conf/extTables.php
 
 # create .htaccess
 cp misc/advanced.htaccess .htaccess
@@ -59,5 +76,5 @@ touch typo3conf/FIRST_INSTALL
 
 # Zip it up
 zip -9r -q ../$packagename-$packageversion.zip .htaccess *
-#cd ..
-#rm -rf $packagename-$packageversion
+cd ..
+rm -rf $packagename-$packageversion
