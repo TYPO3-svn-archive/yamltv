05e3e5718a93c8661501bc16b482e056301f5d81
dieterbunkerd <dieterbunkerd@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-14 10:09:44 +0000

    Enhanced realurl_autoconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yamltv/trunk@41141 735d13b6-9817-0410-8766-e36946ffe9aa

--- yamltv/Resources/Private/Subpackages/yamltv/Files/typo3conf/realurl_autoconf.php
+++ yamltv/Resources/Private/Subpackages/yamltv/Files/typo3conf/realurl_autoconf.php
@@ -1,9 +1,7 @@
 <?php
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']=array (
-  '_DEFAULT' => 
-  array (
-    'init' => 
-    array (
+  '_DEFAULT' => array (
+    'init' => array (
       'enableCHashCache' => true,
       'appendMissingSlash' => 'ifNotFile,redirect',
       'adminJumpToBackend' => true,
@@ -11,8 +9,17 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']=array (
       'enableUrlEncodeCache' => true,
       'emptyUrlReturnValue' => '/',
     ),
-    'pagePath' => 
-    array (
+    'preVars' => array (
+      0 => array (
+        'GETvar' => 'L',
+        'valueMap' => 
+        array (
+          'de' => '1',
+        ),
+        'noMatch' => 'bypass',
+      ),
+    ),
+    'pagePath' => array (
       'type' => 'user',
       'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
       'spaceCharacter' => '-',
@@ -20,31 +27,15 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']=array (
 			'rootpage_id' => 1,
 			'expireDays' => 3,
     ),
-    'fileName' => 
-    array (
-      'defaultToHTMLsuffixOnPrev' => 0,
-      'acceptHTMLsuffix' => 1,
-      'index' => 
-      array (
-        'print' => 
+    'fixedPostVars' => array (
+      '_DEFAULT' => array (
         array (
-          'keyValues' => 
-          array (
-            'type' => 98,
+          'GETvar' => 'no_cache',
+          'valueMap' => array (
+            'nc'  => 1
           ),
+          'noMatch' => 'bypass',
         ),
-      ),
-    ),
-    'preVars' => 
-    array (
-      0 => 
-      array (
-        'GETvar' => 'L',
-        'valueMap' => 
-        array (
-          1 => '1',
-        ),
-        'noMatch' => 'bypass',
       ),
     ),
     'postVarSets' => array (
@@ -63,6 +54,17 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']=array (
                 'spaceCharacter' => '-',
               ),
             ),
+          ),
+        ),
+      ),
+    ),
+    'fileName' => array (
+      'defaultToHTMLsuffixOnPrev' => 0,
+      'acceptHTMLsuffix' => 1,
+      'index' => array (
+        'print' => array (
+          'keyValues' => array (
+            'type' => 98,
           ),
         ),
       ),
